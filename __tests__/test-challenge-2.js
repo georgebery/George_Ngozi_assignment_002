37b2a404f81db4f023768250fbde7d320945400e
George <94810810+georgebery@users.noreply.github.com>
2022-07-14 23:35:17 +0000

    completed test 1 and test 2 task

--- __tests__/test-challenge-2.js
+++ __tests__/test-challenge-2.js
@@ -1,27 +1,18 @@
-// const assignment = require('../src/assignment.js');
+const assignment = require('../src/assignment.js');
 
-// const { countEvenNumbers } = assignment;
-// const maybe = countEvenNumbers === undefined ? test.skip : test;
+const { countEvenNumbers } = assignment;
+const maybe = countEvenNumbers === undefined ? test.skip : test;
 
-// maybe(
-//     'Challenge - 2 : my countEvenNumbers function can count even numbers in array of numbers',
-//     () => {
+maybe(
+    'Challenge - 2 : my countEvenNumbers function can count even numbers in array of numbers',
+    () => {
 const numbers = [
     17, 0, 67, 41, 49, 21, 1, 86, 76, 2, 54, 14, 93, 84, 14, 77, 35, 7, 29, 64
 ];
-//         expect(countEvenNumbers(numbers)).toBe(9);
-//     }
-// );
-
-let evenNumbers = [];
-
-function countEvenNumbers(a, b) {
-    for (i = 0; i < a.length; i++) {
-        if (a[i] % 2 === 0) {
-            b.push(a[i]);
-        }
+        expect(countEvenNumbers(numbers)).toBe(9);
     }
-    console.log(b.length);
-}
+);
 
-countEvenNumbers(numbers, evenNumbers);
+
+
+
